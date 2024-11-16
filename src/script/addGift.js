26027c3838aef6c1560e83740fd22dc1ae6aebc1
Cristian <adrian.cristian.st@gmail.com>
2024-11-16 10:20:12 -0300

    fix: implementacao dos gifts

--- src/script/addGift.js
+++ src/script/addGift.js
@@ -1,51 +1,37 @@
-function addGift(img, title, price) {
+import gifts from './gifts.js';
+
+function insertGiftHTML(img, title, price) {
+    return `
+      <div class="mt-3" style="border-bottom: 2px solid  #cac4d0; padding-bottom: 10px;">
+            <div class="d-flex justify-content-between align-items-center">
+                <div class="d-flex flex-row align-items-center">
+                    <div class="imageCard img-fluid" style='background-image: url("${img}");'></div>
+                    <div class="d-flex flex-column">
+                        <span>${title}</span>
+                        <div class="d-flex flex-row align-items-center">
+                            <small class="h5">${price}</small>
+                        </div>
+                    </div>
+                </div>
+                <button type="button" class="btn btn-info" data-mdb-ripple-init>Preços</button>
+            </div>
+        </div>
+    `
+}
+
+ function addGift(gifts) {
     setTimeout(() => {
         var card = document.getElementById("cardList");
 
-        card.insertAdjacentHTML("beforeend",` <div class="mt-3" style="border-bottom: 2px solid  #cac4d0; padding-bottom: 10px;">
-
-                    <div class="d-flex justify-content-between align-items-center">
-
-                        <div class="d-flex flex-row align-items-center">
-
-                            <div class="imageCard img-fluid" style='background-image: url("${img}");'></div>
-
-                            <div class="d-flex flex-column">
-
-                                <span>${title}</span>
-
-                                <div class="d-flex flex-row align-items-center">
-                                    <small class="h5">${price}</small>
-                                </div>
-
-                            </div>
-
-
-                        </div>
-
-                        <button type="button" class="btn btn-info" data-mdb-ripple-init">Preços</button>
-
-                    </div>
-
-                </div>`
-        )
-    }, 500)
-};
-
-
-// travesseiros
-addGift("https://altenburg.vtexassets.com/arquivos/ids/162015-800-800?v=638491288439170000&width=800&height=800&aspect=true", 
-        "Travesseiro Altenburg Suporte Firme 50cm x 70cm", 
-        "R$37,90"
-    );
-addGift("https://precolandia.vtexassets.com/arquivos/ids/212652-800-auto?v=638435912891000000&width=800&height=auto&aspect=true",
-        "Travesseiro Mega Nasa 48x68cm - Nap", 
-        "R$74,90"
-    );
+        if (!card) return;
     
+        gifts.forEach(({ img, title, price}) => {
+            const giftHTML = insertGiftHTML(img, title, price);
+    
+            card.insertAdjacentHTML("beforeend", giftHTML);
+        });
+    }, 200)
+  
+}
 
-// panelas
-addGift("https://http2.mlstatic.com/D_NQ_NP_2X_948133-MLB71548572201_092023-F.webp", 
-        "Panela de Fondue 11 peças Monte Verde preta Meridional", 
-        "R$ 159,90"
-    );
+addGift(gifts);
---
+++ src/script/gifts.js
@@ -0,0 +1,80 @@
+const gifts = [
+    {
+        img: "https://altenburg.vtexassets.com/arquivos/ids/162015-800-800?v=638491288439170000&width=800&height=800&aspect=true",
+        title: "Travesseiro Altenburg Suporte Firme 50cm x 70cm",
+        price: "R$37,90",
+    },
+  
+    {
+        img: "https://precolandia.vtexassets.com/arquivos/ids/212652-800-auto?v=638435912891000000&width=800&height=auto&aspect=true",
+        title: "Travesseiro Mega Nasa 48x68cm - Nap",
+        price: "R$74,90",
+    },
+
+    {
+        img: "https://http2.mlstatic.com/D_NQ_NP_2X_948133-MLB71548572201_092023-F.webp",
+        title: "Panela de Fondue 11 peças Monte Verde preta Meridional",
+        price: "R$ 159,90"
+    },
+
+    {
+        img: "https://ocapi.nadir.com.br/on/demandware.static/-/Sites-nadir-main-catalog/default/dw08b05b92/nadir/zoom/70640201414660_a_1000px.webp",
+        title: "Jogo de Copos Long Drink Dubai Nadir 500ml com 6 peças",
+        price: "R$54,90",
+    },
+
+    {
+        img: "https://precolandia.vtexassets.com/arquivos/ids/245328-800-auto?v=638519836002500000&width=800&height=auto&aspect=true",
+        title: "Conjunto 2 Copos de Chá Moderno 260mL - Lyor",
+        price: "R$ 47,90",
+    },
+
+    {
+        img: "https://precolandia.vtexassets.com/arquivos/ids/206543-800-auto?v=638038821721130000&width=800&height=auto&aspect=true",
+        title: "Conjunto de Taças De Sobremesa Lights 6 Peças - Nadir Figueiredo",
+        price: "R$ 44,90",
+    },
+
+    {
+        img: "https://precolandia.vtexassets.com/arquivos/ids/208732-800-auto?v=638054411834530000&width=800&height=auto&aspect=true",
+        title: "Jogo de Taças De Água Barone 6 Peças - Nadir Figueiredo",
+        price: "R$ 66,90",
+        link: "https://www.precolandia.com.br/jogo-de-tacas-de-agua-barone-6-pecas-nadir-figueiredo-846899/p",
+    },
+
+    {
+        img: "https://precolandia.vtexassets.com/arquivos/ids/227684-800-auto?v=638509510324570000&width=800&height=auto&aspect=true",
+        title: "Jarra com Tampa em Bambu Sense 1,5l",
+        price: "R$ 143,90",
+        link: "https://www.precolandia.com.br/jarra-com-tampa-em-bambu-sense-15l--072354/p",
+    },
+
+    {
+        img: "https://precolandia.vtexassets.com/arquivos/ids/205159-800-auto?v=638038813078730000&width=800&height=auto&aspect=true",
+        title: "Coqueteleira 500mL - Hercules",
+        price: "R$ 44,90",
+        link: "https://www.precolandia.com.br/coqueteleira-500ml-hercules-563331/p",
+    },
+
+    {
+        img: "https://precolandia.vtexassets.com/arquivos/ids/206946-800-auto?v=638334305826530000&width=800&height=auto&aspect=true",
+        title: "Prato de Melamina 19cm Preto - Marcamix",
+        price: "R$ 23,90",
+        link: "https://www.precolandia.com.br/prato-de-melamina-19cm-preto-marcamix-923974/p",
+    },
+
+    {
+        img: "https://precolandia.vtexassets.com/arquivos/ids/210965-800-auto?v=638309249306530000&width=800&height=auto&aspect=true",
+        title: "Petisqueira Bambu 30x14,5cm - Yoi",
+        price: "R$ 52,90",
+        link: "https://www.precolandia.com.br/petisqueira-bambu-30x145cm-yoi-647420/p",
+    },
+    {
+        img: "https://precolandia.vtexassets.com/arquivos/ids/210965-800-auto?v=638309249306530000&width=800&height=auto&aspect=true",
+        title: "Petisqueira Bambu 30x14,5cm - Yoi",
+        price: "R$ 52,90",
+        link: "https://www.precolandia.com.br/petisqueira-bambu-30x145cm-yoi-647420/p",
+    },
+]
+
+export default gifts;
