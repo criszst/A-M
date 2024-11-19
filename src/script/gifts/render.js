863bf5d42faf9904dc947380606657b03186a9a4
Cristian <adrian.cristian.st@gmail.com>
2024-11-18 22:09:24 -0300

    style: removido o link do card list

--- src/script/gifts/render.js
+++ src/script/gifts/render.js
@@ -4,7 +4,7 @@ export function insertGiftHTML(img, title, price, links) {
         <div class="card">
            <div class="bg-image hover-zoom ripple ripple-surface ripple-surface-light" data-mdb-ripple-color="light">
               <img src="${img}" class="w-100 img-fluid imgCardList imgModal" />
-              <a href="#!">
+              <a>
                  <div class="mask" style="background-color: hsla(0, 0%, 0%, 0.2) !important;">
                     <div class="d-flex justify-content-start align-items-end h-100">
                        <h5><span class="badge ms-2" style="background-color: #463754">R$ ${price}</span></h5>
@@ -16,10 +16,10 @@ export function insertGiftHTML(img, title, price, links) {
               </a>
            </div>
            <div class="card-body">
-              <a href="" class="text-reset">
+              <div class="text-reset">
                  <h6 class="card-title mb-3">${title}</h6>
-              </a>
-              <a href="" class="text-reset">
+              </div>
+              <a class="text-reset">
                  <p></p>
               </a> 
      
