8b5a81c6d1fb1905901c970e967479a9beafa220
Cristian <adrian.cristian.st@gmail.com>
2024-11-23 09:12:50 -0300

    refactor: retirado o loader

--- src/script/loader.js
+++ src/script/loader.js
@@ -1,10 +1,5 @@
-$(window).on('load', function () {
-    $('.loader').fadeOut(); // Esconde a loader
-    $('body').delay(1050).css({ 'overflow': 'visible' }); // Torna o scroll visível novamente
-});
-
 ScrollReveal().reveal('.scroolRev', {
-    duration: 3000,
+    duration: 2000,
     distance: '200px',
     delay:200
 })
